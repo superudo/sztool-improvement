d4b567710e5bc1c440d9be9966caabb15d50ff39
Udo Woitek <Udo.Woitek@gmail.com>
2018-03-10 09:43:40 +0100

    add tslint

--- src/app/TimeSelectorApp.ts
+++ src/app/TimeSelectorApp.ts
@@ -1,75 +1,81 @@
-import { TimesParagraphWrapper } from "../components/TimesParagraphWrapper";
-import * as LocalStorageService from '../environment/LocalStorageService'
-import { TimeSelector } from "../components/TimeSelector";
-import { TimeControlWrapper } from "../components/TimeControlWrapper";
-import { normalize, setupPage } from "csstips";
-import { ControlSwitcher, USE_CONTROL_ITEM } from "../components/ControlSwitcher";
+import { normalize, setupPage } from 'csstips';
+
+import { ControlSwitcher, USE_CONTROL_ITEM } from '../components/ControlSwitcher';
+import { TimeControlWrapper } from '../components/TimeControlWrapper';
+import { TimeSelector } from '../components/TimeSelector';
+import { TimesParagraphWrapper } from '../components/TimesParagraphWrapper';
+import * as LocalStorageService from '../environment/LocalStorageService';
 
 export interface IRunnnable {
-    run: () => void;
+  run: () => void;
 }
+
 export class TimeSelectorApp {
-    private timesParagraphWrapper: TimesParagraphWrapper;
-    private applicationID: string;
-    private that: TimeSelectorApp;
+  private timesParagraphWrapper: TimesParagraphWrapper;
+  private applicationID: string;
+  private that: TimeSelectorApp;
 
-    constructor(appElementID: string) {
-        this.applicationID = appElementID;
-        this.that = this;
+  constructor(appElementID: string) {
+    this.applicationID = appElementID;
+    this.that = this;
+  }
+
+  init(): IRunnnable {
+    if (!LocalStorageService.isSupported) {
+      throw new Error("Local storage not supported.");
     }
 
-    init(): IRunnnable {
-        if (!LocalStorageService.isSupported) {
-            throw new Error("Local storage not supported.");
-        }        
+    this.timesParagraphWrapper = this.getTimesParagraph();
+    return this.that;
+  }
 
-        this.timesParagraphWrapper = this.getTimesParagraph();
-        return this.that;
+  run() {
+    if (this.showTimeSelector()) {
+      normalize();
+      setupPage(this.applicationID);
+      this.prepareApplicationRoot(
+        this.timesParagraphWrapper.getParagraphElement()
+      );
+      this.injectControl();
+    } else {
+      this.injectControlSwitch();
+    }
+  }
+
+  private getTimesParagraph(): TimesParagraphWrapper {
+    if (document.getElementsByClassName("times").length === 0) {
+      throw new Error('Target element witth class "times" not found.');
     }
 
-    run() {
-        if (this.showTimeSelector()) {
-            normalize();
-            setupPage(this.applicationID);
-            this.prepareApplicationRoot(
-                this.timesParagraphWrapper.getParagraphElement());
-                this.injectControl();
-        }
-        else {
-            this.injectControlSwitch();
-        }
-    }
+    return new TimesParagraphWrapper(document.getElementsByClassName(
+      "times"
+    )[0] as HTMLParagraphElement);
+  }
 
-    private getTimesParagraph(): TimesParagraphWrapper {
-        if (document.getElementsByClassName('times').length === 0) {
-            throw new Error('Target element witth class "times" not found.');
-        }
+  private prepareApplicationRoot(insertAfter: HTMLElement) {
+    let appRoot = document.createElement("div");
+    appRoot.id = "app-root";
+    insertAfter.parentNode.insertBefore(appRoot, insertAfter.nextSibling);
+  }
 
-        return new TimesParagraphWrapper(
-            document.getElementsByClassName('times')[0] as HTMLParagraphElement);
-    }
+  private injectControl() {
+    let controls = this.timesParagraphWrapper.getSelectControls();
+    new TimeSelector(
+      new TimeControlWrapper(controls.fromHours, controls.fromMinutes),
+      new TimeControlWrapper(controls.toHours, controls.toMinutes),
+      this.timesParagraphWrapper.getInputButton(),
+      this.timesParagraphWrapper.getCancelButton()
+    ).initApp("app-root");
+    this.timesParagraphWrapper.hideParagraph();
+  }
 
-    private prepareApplicationRoot(insertAfter: HTMLElement) {
-        let appRoot = document.createElement('div');
-        appRoot.id = 'app-root';
-        insertAfter.parentNode.insertBefore(appRoot, insertAfter.nextSibling);
-    }
-    
-    private injectControl() {   
-        let controls = this.timesParagraphWrapper.getSelectControls();
-        new TimeSelector( 
-          new TimeControlWrapper(controls.fromHours, controls.fromMinutes), 
-          new TimeControlWrapper(controls.toHours, controls.toMinutes),
-          this.timesParagraphWrapper.getInputButton(),
-          this.timesParagraphWrapper.getCancelButton()
-        ).initApp('app-root');
-        this.timesParagraphWrapper.hideParagraph();
-    }
-        
-    private showTimeSelector(): boolean {
-        return LocalStorageService.hasItem(USE_CONTROL_ITEM);
-    }
+  private showTimeSelector(): boolean {
+    return LocalStorageService.hasItem(USE_CONTROL_ITEM);
+  }
 
-    private injectControlSwitch(): void {
-        new ControlSwitcher().injectOnSwitch(this.timesParagraphWrapper.getParagraphElement());    }
+  private injectControlSwitch(): void {
+    new ControlSwitcher().injectOnSwitch(
+      this.timesParagraphWrapper.getParagraphElement()
+    );
+  }
 }
--- src/components/TimeSelector.ts
+++ src/components/TimeSelector.ts
@@ -1,65 +1,77 @@
-import { TimeTable } from "./TimeTable";
-import { TimeControlWrapper } from "./TimeControlWrapper";
-import { style } from "typestyle";
-import { ControlSwitcher } from "./ControlSwitcher";
 import { css } from "../styles/ComponentStyles";
+import { ControlSwitcher } from "./ControlSwitcher";
+import { TimeControlWrapper } from "./TimeControlWrapper";
+import { TimeTable } from "./TimeTable";
 
-const INPUT_BUTTON_SIGN: string ='➽';
-const CANCEL_BUTTON_SIGN: string = '⛔';
+const INPUT_BUTTON_SIGN: string = "➽";
+const CANCEL_BUTTON_SIGN: string = "⛔";
 
 export class TimeSelector {
-    fromTable: TimeTable;
-    toTable: TimeTable;
-    private fromTime: TimeControlWrapper;
-    private toTime: TimeControlWrapper;
-    private inputButton: HTMLInputElement;
-    private cancelButton: HTMLInputElement;
+  fromTable: TimeTable;
+  toTable: TimeTable;
+  private fromTime: TimeControlWrapper;
+  private toTime: TimeControlWrapper;
+  private inputButton: HTMLInputElement;
+  private cancelButton: HTMLInputElement;
 
-    constructor(from: TimeControlWrapper, to: TimeControlWrapper, 
-        inputButton: HTMLInputElement, cancelButton: HTMLInputElement) {
-        this.fromTime = from;
-        this.toTime = to;
-        this.inputButton = inputButton;
-        this.cancelButton = cancelButton;
+  constructor(
+    from: TimeControlWrapper,
+    to: TimeControlWrapper,
+    inputButton: HTMLInputElement,
+    cancelButton: HTMLInputElement
+  ) {
+    this.fromTime = from;
+    this.toTime = to;
+    this.inputButton = inputButton;
+    this.cancelButton = cancelButton;
+  }
+
+  public initApp(appRootId: string): void {
+    const fromControl = new TimeTable(this.fromTime);
+    const toControl = new TimeTable(this.toTime);
+
+    const fromView = fromControl.createDom(
+      this.cancelButton,
+      CANCEL_BUTTON_SIGN
+    );
+    let toView = toControl.createDom(this.inputButton, INPUT_BUTTON_SIGN);
+
+    let controlDiv = document.getElementById(appRootId);
+    if (controlDiv === null) {
+      throw "App root not found.";
     }
+    controlDiv.classList.add(css.controlArea);
+    controlDiv.appendChild(fromView);
+    controlDiv.appendChild(toView);
 
-    initApp(appRootId: string): void {
-        let fromControl = new TimeTable(this.fromTime);
-        let toControl = new TimeTable(this.toTime);
+    new ControlSwitcher().injectOffSwitch(toView.parentElement);
 
-        let fromView = fromControl.createDom(this.cancelButton, CANCEL_BUTTON_SIGN);
-        let toView = toControl.createDom(this.inputButton, INPUT_BUTTON_SIGN);
+    this.fromTime.setTimeCheckCallback(this.checkTimes, this);
+    this.toTime.setTimeCheckCallback(this.checkTimes, this);
+  }
 
-        let controlDiv = document.getElementById(appRootId);
-        if (controlDiv === null) {
-            throw "App root not found.";
-        }
-        controlDiv.classList.add(css.controlArea);
-        controlDiv.appendChild(fromView);
-        controlDiv.appendChild(toView);
+  private checkTimes(s: TimeSelector) {
+    let from = s.fromTime.getTimeInMinutes();
+    const to = s.toTime.getTimeInMinutes();
+    const isInvalid = from >= to;
+    s.fromTime.indicateError(isInvalid);
+    s.toTime.indicateError(isInvalid);
+    s.inputButton.disabled = isInvalid;
+  }
 
-        new ControlSwitcher().injectOffSwitch(toView.parentElement);
-
-        this.fromTime.setTimeCheckCallback(this.checkTimes, this);
-         this.toTime.setTimeCheckCallback(this.checkTimes, this);
-    }
-        
-    private checkTimes(s: TimeSelector) {
-        let from = s.fromTime.getTimeInMinutes();
-        let to = s.toTime.getTimeInMinutes();
-        let isInvalid = (from >= to);
-        s.fromTime.indicateError(isInvalid);
-        s.toTime.indicateError(isInvalid);
-        s.inputButton.disabled = isInvalid;
-    }
-
-    private checkTargetTime(fromHour?: string, fromMinutes?: string, toHour?: string, toMinutes?: string): boolean {
-        let fromH = (fromHour !== null)? Number(fromHour): this.fromTime.getHours();
-        let fromM = (fromMinutes !== null)? Number(fromMinutes): this.fromTime.getMinutes();
-        let toH = (toHour !== null)? Number(toHour): this.toTime.getHours();
-        let toM = (toMinutes !== null)? Number(toMinutes): this.toTime.getMinutes();
-        return (60 * fromH + fromM < 60 * toH + toM);
-    }
+  private checkTargetTime(
+    fromHour?: string,
+    fromMinutes?: string,
+    toHour?: string,
+    toMinutes?: string
+  ): boolean {
+    const fromH =
+      fromHour !== null ? Number(fromHour) : this.fromTime.getHours();
+    const fromM =
+      fromMinutes !== null ? Number(fromMinutes) : this.fromTime.getMinutes();
+    const toH = toHour !== null ? Number(toHour) : this.toTime.getHours();
+    const toM =
+      toMinutes !== null ? Number(toMinutes) : this.toTime.getMinutes();
+    return 60 * fromH + fromM < 60 * toH + toM;
+  }
 }
-
-
